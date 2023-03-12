ac4d7234196b47eaeb1d0a61a70ef1529c3a84f6
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-03-13 03:53:24 +0500

    Model: added user model

---
+++ config/strings.js
@@ -0,0 +1,8 @@
+const Strings = {
+  pleaseAddName: 'Please add a name',
+  pleaseAddEmail: 'Please add an email',
+  pleaseAddUsername: 'Please add a username',
+  pleaseAddPhoneNumber: 'Please add a phone number',
+  pleaseAddPassword: 'Please add a password',
+}
+module.exports = Strings
---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const Strings = require('../config/strings')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        length: 20,
+        required: [true, Strings.pleaseAddName],
+        minlength: 3
+    },
+    username: {
+      type: String,
+      required: [true, Strings.pleaseAddUsername],
+      unique: true,
+      length: 30
+  }
+    ,
+    email: {
+        type: String,
+        required: [true, Strings.pleaseAddEmail],
+        unique: true,
+        length: 30,
+        match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+    },
+    phone_number: {
+      type: String,
+      required: [true, Strings.pleaseAddPhoneNumber],
+      unique: true,
+      length: 15,
+      match:  /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/
+  },
+    password: {
+        type: String,
+        required: [true, Strings.pleaseAddPassword]
+    },
+    image: {
+        type: String,
+        default: null
+    }
+},
+{
+    timestamps: true
+}
+)
+
+module.exports = mongoose.model('User', UserSchema)
+
