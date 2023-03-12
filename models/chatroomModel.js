a18356433c049f91859a699cc5b76aa9d01a302d
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-03-13 04:04:12 +0500

    Model: Added Chatroom Model

---
+++ models/chatroomModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Strings = require('../config/strings')
+
+const ChatroomSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+  },
+},
+{
+    timestamps: true
+}
+)
+
+module.exports = mongoose.model('Chatroom', ChatroomSchema)
+
